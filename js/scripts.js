11037a4c9c68294df535e3dbba169693e27d2da1
Grzegorz Koza <kozagrzegorz@interia.pl>
2017-11-23 11:40:34 +0100

    Change gitignore

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 // scripts.js
-
+/*alert('test');*/
 var name = prompt('Enter your name');
 alert('Hello, ' + name);
 console.log('Hello, ' + name);
+
+//alert('Hello, ' + name);
+/*console.log('Hello, ' + name);*/
